7e580cf27f472085efdd01b3925be97a6ca86269
Glyph <glyph@twistedmatrix.com>
2022-05-06 16:43:30 -0700

    swift stub generator so I can draw some UI

---
+++ generate_swift_stub.py
@@ -0,0 +1,67 @@
+from Foundation import NSObject
+from typing import Type, Iterable
+
+from textwrap import dedent
+
+ACTION_SIGNATURE = b"v@:@"
+
+def getOutlets(cls):
+    for k, v in cls.__dict__.items():
+        if getattr(v, "__isOutlet__", False):
+            yield k
+
+
+def getActions(cls):
+    for k, v in cls.__dict__.items():
+        if k.endswith("_") and k.count("_") == 1 and k != 'forwardInvocation_':
+            signature = getattr(v, "signature", None)
+            if signature is None:
+                continue
+            if signature == ACTION_SIGNATURE:
+                yield k[:-1]
+
+
+def fakeSwiftClass(cls: Type[NSObject]) -> str:
+    someOutlets = list(getOutlets(cls))
+    someActions = list(getActions(cls))
+    if not (someOutlets or someActions):
+        return ""
+    indentation = "\n            "
+    outlets = indentation.join(f"@IBOutlet var {each}: id;" for each in someOutlets)
+    actions = indentation.join(
+        [f"@IBAction func {each}(_ sender: NSObject) {{ }}" for each in someActions]
+    )
+    return dedent(
+        f"""
+        class {cls.__name__}: NSObject {{
+            # Outlets
+            {outlets}
+            # Actions
+            {actions}
+        }}
+        """
+    )
+
+
+def swiftFileForInterfaceBuilder(everything: Iterable[object]) -> str:
+    return "".join(
+        [
+            "import Foundation\n",
+            *[
+                fakeSwiftClass(cls)
+                for cls in everything
+                if isinstance(cls, type)
+                and issubclass(cls, NSObject)
+                and cls is not NSObject
+            ],
+        ]
+    )
+
+
+if __name__ == '__main__':
+    import sys
+    from importlib import import_module
+    everything: list[object] = []
+    for module_name in sys.argv[1:]:
+        everything.extend(import_module(module_name).__dict__.values())
+    print(swiftFileForInterfaceBuilder(everything))
