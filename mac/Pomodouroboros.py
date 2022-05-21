80fb119d2890576fe9dbcf80caaf8c69072c3097
Glyph <glyph@twistedmatrix.com>
2022-05-20 23:10:54 -0700

    entitlements file and hard-coded hacks for py2app

--- mac/Pomodouroboros.py
+++ mac/Pomodouroboros.py
@@ -1,4 +1,13 @@
 
+# Static imports to convince py2app to work properly
+
+import OpenSSL.SSL as _
+import pkg_resources.extern.pyparsing as _
+import pkg_resources._vendor.jaraco.text as _
+import service_identity as _
+import six.moves as _
+import _cffi_backend as _
+
 from pomodouroboros.mac_gui import main
 
 main.runMain()
