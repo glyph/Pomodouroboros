84004cc347c009442db06a7e1d5da17b34b20a59
Glyph <glyph@twistedmatrix.com>
2023-03-04 13:13:26 -0800

    let's try this again

--- mac/Pomodouroboros.py
+++ mac/Pomodouroboros.py
@@ -2,8 +2,6 @@
 # Static imports to convince py2app to work properly
 
 import OpenSSL.SSL as _
-import pkg_resources.extern.pyparsing as _
-import pkg_resources._vendor.jaraco.text as _
 import service_identity as _
 import six.moves as _
 import _cffi_backend as _
