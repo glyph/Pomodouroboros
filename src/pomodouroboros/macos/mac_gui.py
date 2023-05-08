27825a55a1f0e0be31871f5dab0bb5207fc93839
Glyph <glyph@twistedmatrix.com>
2023-05-07 22:43:43 -0700

    bind some more stuff

--- src/pomodouroboros/macos/mac_gui.py
+++ src/pomodouroboros/macos/mac_gui.py
@@ -23,6 +23,7 @@ from AppKit import (
     NSTableView,
     NSTextField,
     NSTextFieldCell,
+    NSTextView,
     NSWindow,
 )
 from Foundation import NSIndexSet, NSObject
@@ -150,6 +151,27 @@ class SessionDataSource(NSObject):
     """
 
 
+class TabOrderFriendlyTextViewDelegate(NSObject):
+    """
+    Act as a NSTextViewDelegate to allow for tab/backtab (i.e. shift-tab) to
+    cycle through focus elements, since we're not going to be putting literal
+    tabs into descriptions.
+    """
+
+    def textView_doCommandBySelector_(
+        self, aTextView: NSTextView, aSelector: str
+    ) -> bool:
+        match aSelector:
+            case "insertTab:":
+                aTextView.window().selectNextKeyView_(None)
+                return True
+            case "insertBacktab:":
+                aTextView.window().selectPreviousKeyView_(None)
+                return True
+            case _:
+                return False
+
+
 class IntentionRow(NSObject):
     """
     A row in the intentions table; ObjC wrapper for L{IntentionRow}.
@@ -176,6 +198,10 @@ class IntentionRow(NSObject):
     intention: Intention = objc.object_property()
     shouldHideEstimate = objc.object_property()
     canEditSummary = objc.object_property()
+
+    hasColor = objc.object_property()  # this one probably needs to be
+                                       # forwarded once we get color support
+                                       # into the intention model
 
     forwarded = Forwarder("intention").forwarded
 
