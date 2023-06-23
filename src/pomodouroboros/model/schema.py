41e8ea31275c1594d9538dd38d6bd8edf02d1005
Glyph <code@glyph.im>
2023-06-23 15:59:55 -0700

    separate schema & storage to make schema changes easier to see

---
+++ src/pomodouroboros/model/schema.py
@@ -0,0 +1,105 @@
+from typing import Literal, TypedDict, Union
+
+
+SavedIntervalType = Literal["Pomodoro", "GracePeriod", "Break", "StartPrompt"]
+SavedEstimate = TypedDict(
+    "SavedEstimate",
+    {
+        "duration": float,
+        "madeAt": float,
+    },
+)
+SavedPomodoroID = str
+SavedIntentionID = str
+
+SavedIntention = TypedDict(
+    "SavedIntention",
+    {
+        "created": float,
+        "modified": float,
+        "description": str,
+        "estimates": list[SavedEstimate],
+        # "pomodoros": list[SavedPomodoroID],
+        # Pomodoros are ommitted here because it is an invariant they must
+        # exclusively appear in this list in the order that they are referenced
+        # in the pomodoros themselves, so we can reconstitute that list while
+        # loading them.
+        "abandoned": bool,
+        "title": str,
+        "id": SavedIntentionID,
+    },
+)
+
+SavedBreak = TypedDict(
+    "SavedBreak",
+    {
+        "startTime": float,
+        "endTime": float,
+        "intervalType": Literal["Break"],
+    },
+)
+SavedEvaluationResult = Literal[
+    "distracted", "interrupted", "focused", "achieved"
+]
+SavedEvaluation = TypedDict(
+    "SavedEvaluation",
+    {
+        "result": SavedEvaluationResult,
+        "timestamp": float,
+    },
+)
+SavedPomodoro = TypedDict(
+    "SavedPomodoro",
+    {
+        "startTime": float,
+        "intentionID": SavedIntentionID,
+        "endTime": float,
+        "evaluation": SavedEvaluation | None,
+        "indexInStreak": int,
+        "intervalType": Literal["Pomodoro"],
+    },
+)
+
+SavedGracePeriod = TypedDict(
+    "SavedGracePeriod",
+    {
+        "startTime": float,
+        "originalPomEnd": float,
+        "intervalType": Literal["GracePeriod"],
+    },
+)
+
+SavedStartPrompt = TypedDict(
+    "SavedStartPrompt",
+    {
+        "startTime": float,
+        "endTime": float,
+        "pointsBeforeLoss": float,
+        "pointsAfterLoss": float,
+        "intervalType": Literal["StartPrompt"],
+    },
+)
+SavedSession = TypedDict("SavedSession", {"start": float, "end": float})
+
+SavedInterval = Union[
+    SavedPomodoro, SavedBreak, SavedGracePeriod, SavedStartPrompt
+]
+SavedDuration = TypedDict(
+    "SavedDuration", {"intervalType": SavedIntervalType, "seconds": float}
+)
+SavedStreak = list[SavedInterval]
+
+SavedNexus = TypedDict(
+    "SavedNexus",
+    {
+        "initialTime": float,
+        "intentions": list[SavedIntention],
+        "intervalIsActive": bool,
+        "lastUpdateTime": float,
+        "upcomingDurations": list[SavedDuration],
+        "streaks": list[SavedStreak],
+        "sessions": list[SavedSession],
+    },
+)
+
+
